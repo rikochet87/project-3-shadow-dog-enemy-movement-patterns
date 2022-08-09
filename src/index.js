8c70b08446186a20089fece47ccfb77e360393aa
matias <matias3282@hotmail.com>
2022-08-09 18:30:26 -0300

    enemy movement patterns

--- src/index.js
+++ src/index.js
@@ -13,26 +13,43 @@ function createImage(imageSrc) {
   return image;
 }
 
+
 const enemyImage = createImage(img1);
+let gameFrame = 0;
 
 class Enemy {
   constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.speed = Math.random() * 4 - 2;
+    // this.speed = Math.random() * 4 - 2;
     this.spriteWidth = 83;
     this.spriteHeight = 44;
-    this.width = this.spriteWidth 
-    this.height = this.spriteHeight
-    this.frame = 0
+    this.width = this.spriteWidth;
+    this.height = this.spriteHeight;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
   }
   update() {
-    this.x += this.speed;
-    this.y += this.speed;
+    this.x += Math.random() * 5 - 2.5; 
+    this.y += Math.random() * 5 - 2.5;
+
+    //animate sprites
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
   }
   draw() {
-    ctx.strokeRect(this.x, this.y, this.width, this.height);
-    ctx.drawImage(enemyImage, this.frame*this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+    ctx.drawImage(
+      enemyImage,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 
@@ -46,6 +63,7 @@ function animate() {
     enemy.update();
     enemy.draw();
   });
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 animate();
